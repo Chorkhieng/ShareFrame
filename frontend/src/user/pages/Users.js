0db148e7c9df92712fb3b574468934abb9f4e17c
Chork Hieng <hiengchorknu@gmail.com>
2024-05-20 10:16:40 -0500

    update components

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-    return <h2>Users Work</h2>
+    const USERS = [{id: 'u001', name: 'Chork', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png', places: 2}];
+
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
