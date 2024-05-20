32f25a79300e567e8c1d9975347838165045c7ac
Chork Hieng <hiengchorknu@gmail.com>
2024-05-20 11:45:48 -0500

    fix bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
 import Users from './user/pages/Users';
-import NewPlace from './places/pages/Place';
+import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
-function App() {
+const App = () => {
   return (
-  <Router>
-    <MainNavigation />
-    <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-
-        <Route path="/places/new" exact>
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
             <NewPlace />
-        </Route>
-
-        <Redirect to="/"></Redirect>
-      </Switch>
-    </main>
-    
-    
-  </Router>);
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 };
 
 export default App;
--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,10 +1,19 @@
 import React from 'react';
+
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [{id: 'u001', name: 'Chork', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png', places: 2}];
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Chork',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Pencil_drawing_of_a_girl_in_ecstasy.jpg/800px-Pencil_drawing_of_a_girl_in_ecstasy.jpg',
+      places: 2
+    }
+  ];
 
-    return <UsersList items={USERS} />;
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
