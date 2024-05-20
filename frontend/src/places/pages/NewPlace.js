2a22dbd5a6dc4b3a2341d4ff2fedde92d38a0e54
Chork Hieng <hiengchorknu@gmail.com>
2024-05-20 17:38:02 -0500

    expand inputs

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
@@ -6,13 +6,32 @@ import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {
+    
+  }, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {
+    
+  }, []);
+
   return <form className="place-form">
     <Input 
-      element="input"
-      type="text" 
-      label="title" 
+      id="title"
+      type="text"
+      element="textarea"
+      label="Title" 
       validators={[VALIDATOR_REQUIRE()]}
       errorText="Please enter a valid title." 
+      onInput={titleInputHandler}
+    />
+
+    <Input 
+      id="description"
+      element="textarea"
+      label="Description" 
+      validators={[VALIDATOR_REQUIRE()]}
+      errorText="Please enter a valid description." 
+      onInput={descriptionInputHandler}
     />
   </form>
 };
