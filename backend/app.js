ddda1a38ce4a1095dccb265763f798f34f20fbe9
Chork Hieng <hiengchorknu@gmail.com>
2024-05-21 22:05:27 -0500

    add route-not-exist handling

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/place_routes');
+const HTTPError = require('./models/htttp_error');
 
 const app = express();
 
 app.use(bodyParser.json())
 
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+    const err = new HTTPError("Could not find this route.", 404);
+    throw err;
+});
 
 //middleware
 app.use((err, req, res, next) => {
