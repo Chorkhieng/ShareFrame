03f9c821450939ffc1a3692719ae6b54579b76fc
Chork Hieng <hiengchorknu@gmail.com>
2024-05-23 14:13:16 -0500

    update loading-users

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,40 +1,52 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1',
-      name: 'Chork',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Pencil_drawing_of_a_girl_in_ecstasy.jpg/800px-Pencil_drawing_of_a_girl_in_ecstasy.jpg',
-      places: 1
-    },
-    // {
-    //   id: 'u2',
-    //   name: 'Ny',
-    //   image:
-    //     'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Pencil_drawing_of_a_girl_in_ecstasy.jpg/800px-Pencil_drawing_of_a_girl_in_ecstasy.jpg',
-    //   places: 1
-    // }, 
-    // {
-    //   id: 'u3',
-    //   name: 'Savung',
-    //   image:
-    //     'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Pencil_drawing_of_a_girl_in_ecstasy.jpg/800px-Pencil_drawing_of_a_girl_in_ecstasy.jpg',
-    //   places: 1
-    // },
-    // {
-    //   id: 'u4',
-    //   name: 'Sophal',
-    //   image:
-    //     'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Pencil_drawing_of_a_girl_in_ecstasy.jpg/800px-Pencil_drawing_of_a_girl_in_ecstasy.jpg',
-    //   places: 1
-    // }
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS} />;
+  useEffect(() => {
+    const  sendRequest = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:4000/api/users');
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+        setIsLoading(false);
+      }
+      catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+      
+    };
+
+    sendRequest();
+
+  }, []);
+
+  // handling error
+  const errorHandler = () => {
+    setError(null);
+  }
+
+  return (<React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+        {isLoading && (
+        <div className='center'>
+          <LoadingSpinner />
+        </div>)}
+      {!isLoading && loadedUsers && (<UsersList items={loadedUsers} />)}
+    </React.Fragment>);
 };
 
 export default Users;
