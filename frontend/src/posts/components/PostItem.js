3b1aa9b330a73f2549390809b2079db72446b365
Chork Hieng <hiengchorknu@gmail.com>
2024-05-31 13:01:33 -0500

    remove unused function

--- frontend/src/posts/components/PostItem.js
+++ frontend/src/posts/components/PostItem.js
@@ -31,13 +31,6 @@ const PostItem = props => {
     setShowConfirmModal(false);
   };
 
-  // handle date and time
-  const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
-    const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
-    return formattedDate;
-  };
-
   const confirmDeleteHandler = async () => {
     setShowConfirmModal(false);
     try {
